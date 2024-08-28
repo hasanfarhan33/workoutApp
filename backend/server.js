e98915edbb2de3aa44875bdb9f200011351a0af3
Farhan Hasan <49116826+hasanfarhan33@users.noreply.github.com>
2024-08-28 01:18:57 +0100

    Finished adding API endpoints and started frontend

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,33 @@
 require("dotenv").config()
 const express = require("express"); 
+const mongoose = require("mongoose")
+const workoutsRoutes = require("./routes/workouts")
 
 // Express app 
 const app = express()
 
 // Middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // Routes 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to the app"
-    })
-})
+app.use('/api/workouts', workoutsRoutes)
 
-// Listen for requests 
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port", process.env.PORT)
-})
+// Connect to DB
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        // Listen for requests 
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to DB and listening on port", process.env.PORT)
+        })
+
+    })
+    .catch((error) => {
+        console.log(error); 
+    })
+
 
